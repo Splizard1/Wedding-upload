b2cd63fcbe7207cea21cc7c9c59da4ead1ea34ed
Scott <scottrenney@hotmail.com>
2026-07-29 09:40:06 +0100

    Error handling added

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,17 @@
-use axum::{Router, routing::get};
-
+use axum::{Router, response::Html, routing::get};
 #[tokio::main]
-async fn main() {
-    let app = Router::new().route("/", get(home));
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let app = Router::new().route("/", get(index));
 
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
 
     println!("Server running at http://localhost:3000");
 
-    axum::serve(listener, app).await.unwrap();
+    axum::serve(listener, app).await?;
+
+    Ok(())
 }
 
-async fn home() -> &'static str {
-    "Wedding uploader"
+async fn index() -> Html<&'static str> {
+    Html(include_str!("../static/index.html"))
 }
